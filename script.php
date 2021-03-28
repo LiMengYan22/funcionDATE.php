fdf6125388852f1cfe2b7330c02d97fb1314599e
LiMengYan <80227002+yanlimeng@users.noreply.github.com>
2021-03-28 13:00:08 +0200

    Create script.php

---
+++ script.php
@@ -0,0 +1,9 @@
+<?php
+/* Variables en su formato elegido: */
+$hora= date ("h:i:s"); /* H, I, S.. */
+$fecha= date ("j/n/Y"); /* J,N,Y.. */
+  
+/* Saca en pantalla:*/ 
+  echo $hora;
+  echo $fecha;
+?>
